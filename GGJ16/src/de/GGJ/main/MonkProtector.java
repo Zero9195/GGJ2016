a815f1b90a9253166f1a350543717119b4533932
sven <sven@153-078.eduroam.rwth-aachen.de>
2016-01-30 15:37:05 +0100

    added imagefiles

--- GGJ16/src/de/GGJ/main/MonkProtector.java
+++ GGJ16/src/de/GGJ/main/MonkProtector.java
@@ -2,6 +2,7 @@ package de.GGJ.main;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -9,9 +10,18 @@ import org.newdawn.slick.SlickException;
 
 public class MonkProtector extends BasicGame {
 
+	//TODO find appropiate image for monk with alpha value
 	private Image monk = null;
+	private Image fly = null;
+	private Image spider = null;
 	
 	private static final float scaleOfMonk = 0.4f;
+	private static final float scaleOfFly = 0.08f;
+	private static final float scaleOfSpider = 0.3f;
+	
+	private static final String monkImageFile = "resources/monk.jpeg";
+	private static final String flyImageFile = "resources/fly.png";
+	private static final String spiderImageFile = "resources/spider.jpeg";
 	
 	
 	public MonkProtector(String title) {
@@ -20,14 +30,20 @@ public class MonkProtector extends BasicGame {
 
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		float yPos = arg0.getHeight() / 2;
-		float xPos = arg0.getWidth() / 2;
+		float yPos = arg0.getHeight() / 2 - monk.getHeight() / 2 * scaleOfMonk;
+		float xPos = (arg0.getWidth() / 2) - (monk.getWidth() / 2) * scaleOfMonk;
 		monk.draw(xPos, yPos, scaleOfMonk);
+		arg1.setBackground(Color.white);
+		
+		fly.draw(10, 10, scaleOfFly);
+		spider.draw(270, 320, scaleOfSpider);
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
-		monk = new Image("data/monkey-med.jpeg");
+		monk = new Image(monkImageFile);
+		fly = new Image(flyImageFile);
+		spider = new Image(spiderImageFile);
 	}
 
 	@Override
